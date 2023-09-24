6f397ef86a20d0fa34b6e0186f8bd4c61ea974c8
Thiago Antonio <thiagoapsx@gmail.com>
2023-09-24 20:39:10 -0300

    mudança no try, para add outra exceção

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,35 +11,36 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
-
-		
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double initialBalance = sc.nextDouble();
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
-		Account account = new Account(number, holder, initialBalance, withdrawLimit);
-		
-		System.out.println();
-		
-		System.out.print("Enter amount for withdraw: ");
-		double withdraw = sc.nextDouble();
 		
 		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			Account account = new Account(number, holder, initialBalance, withdrawLimit);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			double withdraw = sc.nextDouble();
+					
 			account.withdraw(withdraw);
 			System.out.println("New Balance: " + String.format("%.2f", account.getBalance()));
 		}
 		catch (IllegalArgumentException e) {
 			System.out.println("Withdraw error: " + e.getMessage());		
 		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
 				
 		sc.close();
-
 	}
 
 }
